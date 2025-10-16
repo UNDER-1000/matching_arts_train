55920c367040d605c5c0250e487077374caf13a2
Moshe Winberg (slash) <MosheWinberg@gmail.com>
2025-10-16 18:42:07 +0300

    refactor: update database configuration to use GCP URL and remove Supabase credentials

--- src/config.py
+++ src/config.py
@@ -4,11 +4,21 @@ class Config:
 	paint_model_path = "data/linear_regression_paint.pkl"
 	walls_model_path = "wall_art_model.pt"
 	colors_n_bins = 30
-	supabase_password = "&6a*bFbMm$Ncb#q"
-	db_url = f"postgresql+asyncpg://postgres.lkycsngpgfshrbnuviki:{supabase_password}@aws-1-eu-north-1.pooler.supabase.com:6543/postgres"
 
-	supabase_walls_password = "YW79nvIlYkrCUAOd"
-	db_walls_url = f"postgresql+asyncpg://postgres.upuugapvaolengekcpqb:{supabase_walls_password}@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
+	db_name = "aiarts"
+	db_user_name="moshe"
+	db_password="~%GiYG7REj}s(hDh"
+
+	db_gcp_url = f"postgresql+asyncpg://{db_user_name}:{db_password}@34.76.19.180:5432/{db_name}"
+
+	db_url = db_gcp_url
+	# supabase_password = "&6a*bFbMm$Ncb#q"
+	# db_url = f"postgresql+asyncpg://postgres.lkycsngpgfshrbnuviki:{supabase_password}@aws-1-eu-north-1.pooler.supabase.com:6543/postgres"
+
+
+	# walls_password = "YW79nvIlYkrCUAOd"
+	# db_walls_url = f"postgresql+asyncpg://postgres.upuugapvaolengekcpqb:{walls_password}@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
+	db_walls_url = db_gcp_url
 	images_url = "https://storage.googleapis.com/image-matcher/artworks/"
 	walls_url = "https://storage.googleapis.com/image-matcher/walls/"
 	
